c63b4b69ac13cde16ee27a4d7b702542c136855b
kimjooeun <32117207+kimjooeun@users.noreply.github.com>
2020-09-08 17:09:30 +0900

    Update 위장.py

--- 위장.py
+++ 위장.py
@@ -1,3 +1,4 @@
+# 풀이 1
 from collections import Counter
 def solution(clothes):
     answer = 0
@@ -11,3 +12,11 @@ def solution(clothes):
     return all_possible -1
     
 solution([["yellow_hat", "headgear"], ["blue_sunglasses", "eyewear"], ["green_turban", "headgear"]])
+
+# 풀이 2
+from functools import reduce
+from collections import Counter
+def solution(clothes):
+    cnt = Counter([data for _, data in clothes])
+    print(cnt.values())
+    return reduce(lambda a,b: (a+1)*(b+1)-1, cnt.values())
